48149c5ffbcf08d9839bcf96c32bb37c6c4eaffa
thaissa kailaine <thaissakailaine.ti@gmail.com>
2026-03-25 11:32:47 -0300

    feat: integrando formulário com sistema de cálculo

--- src/components/FormularioSimulador/index.tsx
+++ src/components/FormularioSimulador/index.tsx
@@ -1,11 +1,39 @@
+import calculoAtraso from '../../utils/logisticaCalculadora/calculoAtraso';
+import calculoBacklog from '../../utils/logisticaCalculadora/calculoBacklog';
+import calculoRisco from '../../utils/logisticaCalculadora/calculoRisco';
 import './style.css'
+import { useState } from "react"
 export function FormularioSimulador(){
+    const [pedidos, setPedidos] = useState(0);
+    const [capacidade, setCapacidade] = useState(0);
+    const [tempo, setTempo] = useState(0);
+    const [resultado, setResultado] = useState('')
+
+    const processarCalculo = (e) => {
+        e.preventDefault()
+        const backlog = calculoBacklog({pedidos: pedidos, capacidade: capacidade, dias: tempo})
+        const atraso = calculoAtraso({backlog: backlog, capacidade:capacidade})
+        const risco = calculoRisco({backlog: backlog, capacidade: capacidade})
+        const acumulo = pedidos - capacidade;
+
+        const mensagem = `
+        Com ${pedidos} pedidos/dia e capacidade de ${capacidade}, haverá acúmulo de ${acumulo} pedidos por dia. Em ${tempo} dias, o backlog será de ${backlog} pedidos. Isso representa um atraso médio de aproximadamente ${atraso} dias. Risco: ${risco}.
+        ` 
+        setResultado(mensagem)
+    }
+
     return(
+        <>
         <form className="container-formulario">
-            <input type="number" placeholder="Pedidos por dia" />
-            <input type="number" placeholder="Capacidade por dia" />
-            <input type="number" placeholder="Tempo de operação" />
+            <input type="number" placeholder="Pedidos por dia" onChange={(e) => setPedidos(Number(e.target.value))} />
+            <input type="number" placeholder="Capacidade por dia" onChange={(e) => setCapacidade(Number(e.target.value))} />
+            <input type="number" placeholder="Tempo de operação" onChange={(e) => setTempo(Number(e.target.value))} />
+            <button onClick={processarCalculo} type='button'>Calcular</button>
         </form>
+
+        <p>{resultado}</p>
+       
+        </>
     )
 }
 
